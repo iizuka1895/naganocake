09dc20f90b9cb0b6ffde8ed5ed4bf5a4d66fe031
YuIizuka <148466726+iizuka1895@users.noreply.github.com>
2024-01-03 05:23:43 +0000

    ログイン画面

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  
   def after_sign_in_path_for(resource)
-    public_customer_path(resource.id)
+    root_path
   end
   # before_action :configure_sign_in_params, only: [:create]
 
