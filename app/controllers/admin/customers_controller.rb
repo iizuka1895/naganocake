45d66c4dd57493ded5c1f341c27c7a7c4fcd8153
YuIizuka <148466726+iizuka1895@users.noreply.github.com>
2024-01-08 07:55:29 +0000

    顧客詳細、顧客編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,29 +1,29 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all.page(params[:page])
   end
-  
+
   def show
     @customers = Customer.find(params[:id])
   end
-  
+
   def edit
     @customers = Customer.find(params[:id])
   end
-  
+
   def update
     @customers = Customer.find(params[:id])
-    if @customer.update(customer_params)
+    if @customers.update(customer_params)
       flash[:notice] = "会員情報を更新しました"
       redirect_to admin_customer_path
     else
       render 'edit'
     end
   end
-  
+
   private
    def customer_params
-     params.require(:customer).permit(:first_name,:last_name,:kana_first_name,:kana_last_name,:postal_code,:address,:phone_number,:email,:is_active)
+     params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :adress, :telephone_number, :email, :is_active)
    end
 end
